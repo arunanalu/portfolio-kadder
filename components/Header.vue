30ba6258a4a26f1597e1a7d55da472bf6b24d078
mayu <mayusatori@gmail.com>
2022-10-24 02:35:02 -0300

    style: add font to title

--- components/Header.vue
+++ components/Header.vue
@@ -4,7 +4,14 @@
     justify-center items-center gap-y-14
     "
   >
-    <h1 class="text-5xl">Kadder</h1>
+    <h1 class="main-title text-5xl font-bold">Kadder</h1>
     <CustomNavbar />
   </header>
 </template>
+
+<style scoped>
+  @import url('https://fonts.googleapis.com/css2?family=Cinzel+Decorative:wght@400;700;900&display=swap');
+  .main-title {
+    font-family: 'Cinzel Decorative', cursive;
+  }
+</style> 
