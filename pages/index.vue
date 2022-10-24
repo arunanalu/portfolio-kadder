9953f921048878c7c4286bad4a0feae362e1d51a
mayu <mayusatori@gmail.com>
2022-10-24 15:29:29 -0300

    small changes

--- pages/index.vue
+++ pages/index.vue
@@ -11,8 +11,8 @@
     <div v-if="pending">carregando...</div>
     <div v-else>
       <div v-for="image in value" :key="image.key" >
-        <p>{{ image.description }}</p>
         <img :src="runtimeConfig.public.api + '/image/' + image.key" alt="ilustração" />
+        <p>{{ image.description }}</p>
       </div>
     </div>
   </section>
