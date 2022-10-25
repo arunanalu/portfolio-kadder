5dce7390536a19e895af139dce7b88764ef537b2
mayu <mayusatori@gmail.com>
2022-10-24 21:34:01 -0300

    feat: update main page style and add custom directive to detect click outside component

--- components/ImageItem.vue
+++ components/ImageItem.vue
@@ -2,6 +2,8 @@
 
   const runtimeConfig = useRuntimeConfig()
 
+  const open = ref(false)
+
   defineProps({
     image: {
       type: Object,
@@ -12,9 +14,40 @@
 </script>
 
 <template>
+
+  <Teleport to="body">
+    <div v-if="open" 
+      class="fixed top-0 bg-zinc-900/90 text-gray-100
+      w-screen min-h-screen flex justify-center items-center
+      "
+    >
+      <div 
+        class="w-[50%] h-screen p-4 flex flex-col
+        items-center justify-start relative
+        "
+      >
+      <button 
+        @click="open = false"
+        class="absolute top-4 right-4 scale-[2]"
+      >
+        x
+      </button>
+        <div class="mt-5" v-click-outside="() => open = false">
+          <img 
+            :src="runtimeConfig.public.api + '/image/' + image.key" alt="ilustração" 
+            class="max-w-[1000px] max-h-[700px]"
+          />
+        </div>
+        <p class="mt-10">Descrição:</p>
+        <p>{{ image.description }}</p>
+      </div>
+    </div>
+  </Teleport>
+
+  <a class="cursor-pointer" @click="open = true" >
     <img 
       :src="runtimeConfig.public.api + '/image/' + image.key" alt="ilustração" 
       class="w-full h-full object-cover"
     />
-  <!-- <p>{{ image.description }}</p> -->
+  </a>
 </template>
---
+++ plugins/directives.js
@@ -0,0 +1,18 @@
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.directive('click-outside', {
+    beforeMount(el, binding) {
+      el.clickOutsideEvent = (evt) => {
+        evt.stopPropagation();
+        if (!(el === evt.target || el.contains(evt.target))) {
+          binding.value(evt, el);
+        }
+      };
+      window.requestAnimationFrame(() => {
+        document.addEventListener("click", el.clickOutsideEvent);
+      });
+    },
+    unmounted(el) {
+      document.removeEventListener("click", el.clickOutsideEvent);
+    },
+  })
+})
