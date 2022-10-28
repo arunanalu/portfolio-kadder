20ed3609918216142fd16d3beb7ef9b883aa9348
mayu <mayusatori@gmail.com>
2022-10-28 07:12:45 -0300

    style: modal css

--- components/ImagesModal.vue
+++ components/ImagesModal.vue
@@ -31,38 +31,38 @@ let selectedImage = useState('selectedImage')
 <template>
   <Teleport to="body">
     <div v-if="open"
-      class="fixed top-0 bg-zinc-800/90 text-gray-100
+      class="fixed top-0 bg-zinc-900 text-gray-100
       w-screen min-h-screen flex justify-center items-center fade-in
       z-50
       "
     >
     <button 
-      class="z-50 absolute top-0 right-0 sm:top-5 sm:right-5 md:top-5 md:right-5 xl:top-10 xl:right-20 p-2 rounded-lg bg-gradient-to-br from-purple-700 to-pink-700 "
+      class="z-50 text-sm sm:text-lg md:text-xl absolute top-14 right-8 sm:top-10 sm:right-8 md:top-8 md:right-8 xl:top-10 xl:right-20 p-2 rounded-lg bg-zinc-800 "
       @click="open = false"
     >
       FECHAR
     </button>
     <div 
-      class="absolute flex justify-center items-center left-0 h-screen w-[10%] md:w-[20%] "
+      class="absolute flex justify-center items-center left-0 h-screen w-[8%] md:w-[10%] xl:w-[15%] "
       @click.stop="previousImage(data, selectedImage.key)"
     >
       <button 
-        class="w-10 h-10 bg-gray-100 arrow"
+        class="w-8 h-8 md:w-10 md:h-10 bg-gray-100 arrow"
         >
       </button>
     </div>
     <div 
-      class="absolute flex justify-center items-center right-0 h-screen w-[10%] md:w-[20%]  z-0"
+      class="absolute flex justify-center items-center right-0 h-screen w-[8%] md:w-[10%] xl:w-[15%] z-0"
       @click.stop="nextImage(data, selectedImage.key)"
     >
       <button 
-        class="w-10 h-10 bg-gray-100 bg-gray-100 arrow rotate-180"
+        class="w-8 h-8 md:w-10 md:h-10 bg-gray-100 bg-gray-100 arrow rotate-180"
         >
         next
       </button>
     </div>
       <div
-        class="w-[85%] sm:w-[70%] md:w-[60%] lg:w-[80%] xl:w-[70%] 2xl:w-[80%] flex flex-col lg:flex-row justify-center "
+        class="w-[75%] sm:w-[70%] md:w-[60%] lg:w-[80%] xl:w-[70%] 2xl:w-[80%] flex flex-col lg:flex-row justify-center "
       >
         <div 
           class="flex justify-center items-center ">
@@ -74,10 +74,9 @@ let selectedImage = useState('selectedImage')
               />
         </div>
         <div v-if="selectedImage.description.length > 0" class="flex justify-center items-center ">
-          <div class="h-[180px] md:h-[240px] w-[100%] lg:h-[100%] lg:w-[300px] flex flex-col text-xl 
-            shadow-xl bg"
+          <div class="h-[130px] md:h-[180px] w-[100%] lg:h-[100%] lg:w-[300px] flex flex-col text-xl 
+            bg-zinc-800"
           >
-            <p class="mt-6 sm:mt-10 xl:mt-20 self-center text-base sm:text-lg md:text-xl ">Descrição:</p>
             <p class="mt-1 sm:mt-5 lg:mt-10 w-[100%] h-[100%] p-3 text-center text-base sm:text-lg md:text-xl  ">{{ selectedImage.description }}</p>
           </div>
         </div>
